7e51aeeecad6ea638fc161d6e9b14d9d4f327a8b
Yuki Naka <yukinaka2168@au.com>
2024-11-02 13:02:53 +0900

    made simple version of SettingView.

---
+++ EnglishScoreTracker/Image.swift
@@ -0,0 +1,26 @@
+//
+//  Image.swift
+//  EnglishScoreTracker
+//
+//  Created by 仲優樹 on 2024/11/02.
+//
+
+import SwiftUI
+
+extension Image {
+    /// 画像の基本設定
+    func resizableScaledToFit() -> some View {
+        self
+            .resizable()
+            .scaledToFit()
+    }
+    
+    static let gearshape        = Image(systemName: "gearshape")
+    static let arrowupArrowdown = Image(systemName: "arrow.up.arrow.down")
+    static let plus             = Image(systemName: "plus")
+    static let pencilCircle     = Image(systemName: "pencil.circle")
+    static let plusCircle       = Image(systemName: "plus.circle")
+    static let trash            = Image(systemName: "trash")
+    static let xmark            = Image(systemName: "xmark")
+    static let photoBadgePlus   = Image(systemName: "photo.badge.plus")
+}
--- EnglishScoreTracker/SettingView.swift
+++ EnglishScoreTracker/SettingView.swift
@@ -8,8 +8,84 @@
 import SwiftUI
 
 struct SettingView: View {
+    @Environment(\.dismiss) private var dismiss
+    private let build = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+    
     var body: some View {
-        Text("made feature/SettingView branch.")
+        NavigationStack {
+            listView()
+                .navigationTitle("設定")
+                .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+    
+    @ViewBuilder
+    private func listView() -> some View {
+        List {
+            Section("このアプリについて") {
+                aboutThisAppCellView()
+                contactCellView()
+                reviewCellView()
+                shareThisAppCellView()
+                versionCellView()
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func aboutThisAppCellView() -> some View {
+        HStack {
+            Text("このアプリについて")
+            Spacer()
+            Text(build)
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+        }
+    }
+    
+    @ViewBuilder
+    private func contactCellView() -> some View {
+        HStack {
+            Text("お問い合わせ")
+            Spacer()
+            Image(systemName: "arrowshape.turn.up.right")
+                .font(Font.system(size: 14, weight: .bold))
+                .foregroundStyle(.secondary)
+                .opacity(0.5)
+        }
+    }
+    
+    @ViewBuilder
+    private func reviewCellView() -> some View {
+        HStack {
+            Text("レビューする")
+            Spacer()
+            Text(build)
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+        }
+    }
+    
+    @ViewBuilder
+    private func shareThisAppCellView() -> some View {
+        HStack {
+            Text("このアプリを共有する")
+            Spacer()
+            Text(build)
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+        }
+    }
+    
+    @ViewBuilder
+    private func versionCellView() -> some View {
+        HStack {
+            Text("バージョン")
+            Spacer()
+            Text(build)
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+        }
     }
 }
 
