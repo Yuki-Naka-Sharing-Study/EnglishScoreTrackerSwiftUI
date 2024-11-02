f780360a4dc9fcfea0e91c21be2bf001ff0d635a
Yuki Naka <yukinaka2168@au.com>
2024-11-02 11:28:39 +0900

    made RecordView.

---
+++ EnglishScoreTracker/RecordView.swift
@@ -0,0 +1,18 @@
+//
+//  RecordView.swift
+//  EnglishScoreTracker
+//
+//  Created by 仲優樹 on 2024/11/02.
+//
+
+import SwiftUI
+
+struct RecordView: View {
+    var body: some View {
+        Text("記録画面です。")
+    }
+}
+
+#Preview {
+    RecordView()
+}
