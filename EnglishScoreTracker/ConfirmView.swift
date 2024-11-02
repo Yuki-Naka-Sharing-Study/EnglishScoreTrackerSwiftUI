42876a990461c4a53ccb398faa9811cdd1ab5b8e
Yuki Naka <yukinaka2168@au.com>
2024-11-02 11:27:11 +0900

    made ConfirmView.

---
+++ EnglishScoreTracker/ConfirmView.swift
@@ -0,0 +1,18 @@
+//
+//  ConfirmView.swift
+//  EnglishScoreTracker
+//
+//  Created by 仲優樹 on 2024/11/02.
+//
+
+import SwiftUI
+
+struct ConfirmView: View {
+    var body: some View {
+        Text("記録確認画面です。")
+    }
+}
+
+#Preview {
+    ConfirmView()
+}
