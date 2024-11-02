07346325e261a2b5afde97c28eb0185e85cd5e07
Yuki Naka <yukinaka2168@au.com>
2024-11-02 14:05:56 +0900

    made Rectangle for eiken niji.

--- EnglishScoreTracker/SelectConfirmView.swift
+++ EnglishScoreTracker/SelectConfirmView.swift
@@ -9,14 +9,25 @@ import SwiftUI
 
 struct SelectConfirmView: View {
     var body: some View {
-        ZStack {
-            Rectangle()
-                .fill(Color.red)
-                .frame(width: 200, height: 200)
-            
-            Text("英検一次")
-                .foregroundColor(.white)
-                .font(.headline)
+        HStack {
+            ZStack {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                
+                Text("英検一次")
+                    .foregroundColor(.white)
+                    .font(.headline)
+            }
+            ZStack {
+                Rectangle()
+                    .fill(Color.orange)
+                    .frame(width: 200, height: 200)
+                
+                Text("英検二次")
+                    .foregroundColor(.white)
+                    .font(.headline)
+            }
         }
     }
 }
