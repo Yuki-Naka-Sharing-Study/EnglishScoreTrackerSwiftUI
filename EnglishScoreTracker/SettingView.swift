19fc4a84052a7bf09b7981ca62ade36959758e1a
Yuki Naka <yukinaka2168@au.com>
2024-11-02 11:30:16 +0900

    made SettingView.

---
+++ EnglishScoreTracker/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  EnglishScoreTracker
+//
+//  Created by 仲優樹 on 2024/11/02.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text("設定画面です。")
+    }
+}
+
+#Preview {
+    SettingView()
+}
