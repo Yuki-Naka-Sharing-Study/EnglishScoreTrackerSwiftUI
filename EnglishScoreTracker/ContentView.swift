f05b294ce33f99309d9597bf478be0ee32cdabe7
Yuki Naka <yukinaka2168@au.com>
2024-11-02 11:23:15 +0900

    made feature/TabView branch.

--- EnglishScoreTracker/ContentView.swift
+++ EnglishScoreTracker/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("made develop branch.")
+            Text("made feature/TabView branch.")
         }
         .padding()
     }
