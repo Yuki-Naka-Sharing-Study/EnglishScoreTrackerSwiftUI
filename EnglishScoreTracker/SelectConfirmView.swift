edd1ca63b4363061e3932734723782bc8c3e5fa9
Yuki Naka <yukinaka2168@au.com>
2024-11-02 14:26:33 +0900

    added NavigationStack to allow screen transitions when each text is tapped.

--- EnglishScoreTracker/SelectConfirmView.swift
+++ EnglishScoreTracker/SelectConfirmView.swift
@@ -9,58 +9,80 @@ import SwiftUI
 
 struct SelectConfirmView: View {
     var body: some View {
-        HStack {
-            ZStack {
-                Rectangle()
-                    .fill(Color.red)
-                    .frame(width: 200, height: 200)
-                Text("英検一次")
-                    .foregroundColor(.white)
-                    .font(.title)
-            }
-            ZStack {
-                Rectangle()
-                    .fill(Color.orange)
-                    .frame(width: 200, height: 200)
-                Text("英検二次")
-                    .foregroundColor(.white)
-                    .font(.title)
-            }
-        }
-        HStack {
-            ZStack {
-                Rectangle()
-                    .fill(Color.yellow)
-                    .frame(width: 200, height: 200)
-                Text("TOEIC")
-                    .foregroundColor(.white)
-                    .font(.title)
-            }
-            ZStack {
-                Rectangle()
-                    .fill(Color.green)
-                    .frame(width: 200, height: 200)
-                Text("TOEIC SW")
-                    .foregroundColor(.white)
-                    .font(.title)
-            }
-        }
-        HStack {
-            ZStack {
-                Rectangle()
-                    .fill(Color.blue)
-                    .frame(width: 200, height: 200)
-                Text("TOEFL iBT")
-                    .foregroundColor(.white)
-                    .font(.title)
-            }
-            ZStack {
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 200, height: 200)
-                Text("IELTS")
-                    .foregroundColor(.white)
-                    .font(.title)
+        NavigationStack {
+            VStack {
+                HStack {
+                    NavigationLink(destination: Text("英検一次の詳細")) {
+                        ZStack {
+                            Rectangle()
+                                .fill(Color.red)
+                                .frame(width: 200, height: 200)
+                            
+                            Text("英検一次")
+                                .foregroundColor(.white)
+                                .font(.title)
+                        }
+                    }
+                    NavigationLink(destination: Text("英検二次の詳細")) {
+                        ZStack {
+                            Rectangle()
+                                .fill(Color.orange)
+                                .frame(width: 200, height: 200)
+                            
+                            Text("英検二次")
+                                .foregroundColor(.white)
+                                .font(.title)
+                        }
+                    }
+                }
+                HStack {
+                    NavigationLink(destination: Text("TOEICの詳細")) {
+                        ZStack {
+                            Rectangle()
+                                .fill(Color.yellow)
+                                .frame(width: 200, height: 200)
+                            
+                            Text("TOEIC")
+                                .foregroundColor(.white)
+                                .font(.title)
+                        }
+                    }
+                    NavigationLink(destination: Text("TOEIC SWの詳細")) {
+                        ZStack {
+                            Rectangle()
+                                .fill(Color.green)
+                                .frame(width: 200, height: 200)
+                            
+                            Text("TOEIC SW")
+                                .foregroundColor(.white)
+                                .font(.title)
+                        }
+                    }
+                }
+                HStack {
+                    NavigationLink(destination: Text("TOEFL iBTの詳細")) {
+                        ZStack {
+                            Rectangle()
+                                .fill(Color.blue)
+                                .frame(width: 200, height: 200)
+                            
+                            Text("TOEFL iBT")
+                                .foregroundColor(.white)
+                                .font(.title)
+                        }
+                    }
+                    NavigationLink(destination: Text("IELTSの詳細")) {
+                        ZStack {
+                            Rectangle()
+                                .fill(Color.purple)
+                                .frame(width: 200, height: 200)
+                            
+                            Text("IELTS")
+                                .foregroundColor(.white)
+                                .font(.title)
+                        }
+                    }
+                }
             }
         }
     }
