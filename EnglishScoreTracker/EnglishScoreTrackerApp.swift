223c2145c9b485694e2dd6453e237483df4a631d
Yuki Naka <yukinaka2168@au.com>
2024-11-02 11:18:21 +0900

    Initial Commit

---
+++ EnglishScoreTracker/EnglishScoreTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishScoreTrackerApp.swift
+//  EnglishScoreTracker
+//
+//  Created by 仲優樹 on 2024/11/02.
+//
+
+import SwiftUI
+
+@main
+struct EnglishScoreTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
