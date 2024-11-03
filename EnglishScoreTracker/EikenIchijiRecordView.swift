342fe0e7fa766a1e4d9f10a11e6c8adf607d2bc7
Yuki Naka <yukinaka2168@au.com>
2024-11-03 18:49:18 +0900

    受験日を選択 Text and DatePicker.

--- EnglishScoreTracker/EikenIchijiRecordView.swift
+++ EnglishScoreTracker/EikenIchijiRecordView.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct EikenIchijiRecordView: View {
+    @State private var selectedDate = Date()
+    
     var body: some View {
-        Text("made feature/EikenIchijiRecordView branch.")
+        HStack {
+            Text("受験日を選択: \(selectedDate, formatter: dateFormatter)")
+                .padding()
+            DatePicker("日付を選択", selection: $selectedDate, displayedComponents: [.date])
+                .datePickerStyle(WheelDatePickerStyle())
+                .frame(maxWidth: 150)
+            
+        }
+        Spacer()
+    }
+}
+
+private var dateFormatter: DateFormatter {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    return formatter
+}
+
+struct DatePickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        EikenIchijiRecordView()
     }
 }
 
