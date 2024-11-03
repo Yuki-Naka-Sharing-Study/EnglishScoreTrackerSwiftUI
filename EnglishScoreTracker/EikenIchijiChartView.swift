70ac630f386f3052638812fbb6d7947327b6c38d
Yuki Naka <yukinaka2168@au.com>
2024-11-03 16:26:12 +0900

    made feature/EikenIchijiChartView branch.

---
+++ EnglishScoreTracker/EikenIchijiChartView.swift
@@ -0,0 +1,18 @@
+//
+//  EikenIchijiChartView.swift
+//  EnglishScoreTracker
+//
+//  Created by 仲優樹 on 2024/11/03.
+//
+
+import SwiftUI
+
+struct EikenIchijiChartView: View {
+    var body: some View {
+        Text("made feature/EikenIchijiChartView branch.")
+    }
+}
+
+#Preview {
+    EikenIchijiChartView()
+}
