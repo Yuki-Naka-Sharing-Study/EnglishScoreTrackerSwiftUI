884d021c36000c171e915a273a84d733eb5a42a0
Yuki Naka <yukinaka2168@au.com>
2024-11-03 16:38:59 +0900

    made feature/EikenIchijiIndividualView branch.

---
+++ EnglishScoreTracker/EikenIchijiIndividualView.swift
@@ -0,0 +1,18 @@
+//
+//  EikenIchijiIndividualView.swift
+//  EnglishScoreTracker
+//
+//  Created by 仲優樹 on 2024/11/03.
+//
+
+import SwiftUI
+
+struct EikenIchijiIndividualView: View {
+    var body: some View {
+        Text("made feature/EikenIchijiIndividualView branch.")
+    }
+}
+
+#Preview {
+    EikenIchijiIndividualView()
+}
