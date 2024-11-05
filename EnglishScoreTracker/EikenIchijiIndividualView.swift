acda0ab667078def483f770ead164f6212e5e582
Yuki Naka <yukinaka2168@au.com>
2024-11-05 10:44:59 +0900

    added padding to EikenIchijiIndividualView.

--- EnglishScoreTracker/EikenIchijiIndividualView.swift
+++ EnglishScoreTracker/EikenIchijiIndividualView.swift
@@ -12,40 +12,48 @@ struct EikenIchijiIndividualView: View {
         NavigationStack {
             HStack {
                 Text("受験日")
+                    .padding(16)
                 Spacer()
                 Text("2024年11月3日")
             }
             HStack {
                 Text("受験級")
+                    .padding(16)
                 Spacer()
                 Text("準一級")
             }
             HStack {
                 Text("スコア")
+                    .padding(16)
                 Spacer()
             }
             HStack {
                 Text("Reading")
+                    .padding(.leading, 32)
                 Spacer()
                 Text("850")
             }
             HStack {
                 Text("Listening")
+                    .padding(.leading, 32)
                 Spacer()
                 Text("850")
             }
             HStack {
                 Text("Writing")
+                    .padding(.leading, 32)
                 Spacer()
                 Text("850")
             }
             HStack {
                 Text("Speaking")
+                    .padding(.leading, 32)
                 Spacer()
                 Text("850")
             }
             HStack {
                 Text("Memo")
+                    .padding(16)
                 Spacer()
                 Text("The topic of Writing was so difficult.")
             }
