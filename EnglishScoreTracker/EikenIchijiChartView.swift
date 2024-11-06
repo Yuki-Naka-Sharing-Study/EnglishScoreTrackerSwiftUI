16d638bded134898081e6c9f721fae87b89b45a7
仲優樹 <yu-ki216m@outlook.jp>
2024-11-06 14:23:40 +0900

    navigate to EikenIchijiIndividualView

--- EnglishScoreTracker/EikenIchijiChartView.swift
+++ EnglishScoreTracker/EikenIchijiChartView.swift
@@ -13,47 +13,47 @@ struct EikenIchijiChartView: View {
     @State private var selectedYaer = 2
     
     var body: some View {
-        // 以下、記録無しの場合
-//        ContentUnavailableView(
-//            "記録無し",
-//            systemImage: "pencil",
-//            description: Text("まだ何も記録されていません。")
-//        )
-        // 以下、記録ありの場合
-        VStack {
-            Text("一級")
-                .bold()
-            Chart(data) {
-                LineMark(
-                    x: .value("Schedule", $0.schedule),
-                    y: .value("Score", $0.score)
-                )
-                .foregroundStyle(by: .value("TypeOfSkill", $0.typeOfSkill))
+        NavigationView {
+            VStack {
+                Text("一級")
+                    .bold()
+                
+                Chart(data) {
+                    LineMark(
+                        x: .value("Schedule", $0.schedule),
+                        y: .value("Score", $0.score)
+                    )
+                    .foregroundStyle(by: .value("TypeOfSkill", $0.typeOfSkill))
+                }
+                .padding()
+                .dynamicTypeSize(.xxLarge)
+                
+                Text("受験した年")
+                
+                Picker("受験した年を選択", selection: $selectedYaer) {
+                    Text("2023").tag(1)
+                    Text("2024").tag(2)
+                    Text("2025").tag(3)
+                }
+                .pickerStyle(.wheel)
+                
+                ForEach(data.first(where: { $0.schedule == "2023/10/8" }) != nil ? [data.first(where: { $0.schedule == "2023/10/8" })!] : []) { item in
+                    NavigationLink(destination: EikenIchijiIndividualView()) {
+                        Text(item.schedule)
+                            .foregroundColor(.blue)
+                            .padding()
+                    }
+                }
             }
-            .padding()
-            .dynamicTypeSize(.xxLarge)
-            
-            Text("受験した年")
-            Picker("受験した年を選択", selection: $selectedYaer) {
-                /// 選択項目の一覧
-                Text("2023").tag(1)
-                Text("2024").tag(2)
-                Text("2025").tag(3)
-            }
-            .pickerStyle(.wheel)
         }
     }
 }
 
-#Preview {
-    EikenIchijiChartView()
-}
-
 struct EikenIchijiInfo: Identifiable {
     let id = UUID()
-    var schedule = ""
-    var score = 0
-    var typeOfSkill = ""
+    var schedule: String
+    var score: Int
+    var typeOfSkill: String
     
     static func fetchData() -> [EikenIchijiInfo] {
         [
@@ -63,15 +63,19 @@ struct EikenIchijiInfo: Identifiable {
             EikenIchijiInfo(schedule: "2023/6/4", score: 690, typeOfSkill: "Reading"),
             EikenIchijiInfo(schedule: "2023/10/8", score: 751, typeOfSkill: "Reading"),
             
-            // Listining
-            EikenIchijiInfo(schedule: "2023/1/21", score: 616, typeOfSkill: "Listining"),
-            EikenIchijiInfo(schedule: "2023/6/4", score: 642, typeOfSkill: "Listining"),
-            EikenIchijiInfo(schedule: "2023/10/8", score: 623, typeOfSkill: "Listining"),
+            // Listening
+            EikenIchijiInfo(schedule: "2023/1/21", score: 616, typeOfSkill: "Listening"),
+            EikenIchijiInfo(schedule: "2023/6/4", score: 642, typeOfSkill: "Listening"),
+            EikenIchijiInfo(schedule: "2023/10/8", score: 623, typeOfSkill: "Listening"),
             
-            // Writiing
+            // Writing
             EikenIchijiInfo(schedule: "2023/1/21", score: 620, typeOfSkill: "Writing"),
             EikenIchijiInfo(schedule: "2023/6/4", score: 669, typeOfSkill: "Writing"),
             EikenIchijiInfo(schedule: "2023/10/8", score: 713, typeOfSkill: "Writing"),
         ]
     }
+}
+
+#Preview {
+    EikenIchijiChartView()
 }
